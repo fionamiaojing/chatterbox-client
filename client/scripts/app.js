9e162fc838d9de716f12a2fb540ebbae386ce1bd
fionamiaojing <fionamiaojing@gmail.com>
2018-05-04 17:10:49 -0700

    Passed tests up to events.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,46 @@
 // YOUR CODE HERE:
+var app = {
+  init: function() {},
+  server: 'http://parse.sfm8.hackreactor.com',
+  fetch: function() {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com',
+      type: 'GET',
+      contentType: 'application/json',
+      // success: function (data) {
+      //   console.log('chatterbox: Message received');
+      // },
+      // error: function (data) {
+      //   // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      //   console.error('chatterbox: Failed to receive message', data);
+      // }
+    });
+  },
+  send: function(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  renderMessage: function(message) {
+    $('<div>'+message+'</div>').appendTo('#chats');
+  },
+  clearMessages: function() { 
+    $('#chats').empty();
+  },
+  renderRoom: function(room) {
+    $('<option>'+room+'</option>').appendTo('#roomSelect');
+  },
+};
+
